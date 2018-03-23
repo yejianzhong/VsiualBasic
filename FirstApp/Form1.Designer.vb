595d1417b9339666f059b710e5d43f4bba6970d2
yjz-pc <yjz-pc@YJZ-PC>
2018-03-23 09:04:49 +0800

    第一次提交

---
+++ FirstApp/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(26, 28)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(228, 74)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "求最小数"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(26, 153)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(228, 76)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "整数判断"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(26, 287)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(228, 76)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "季节查询"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Visual Basic"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ FirstApp/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+
+    '求最小数
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a, b, c, min As Single
+        a = InputBox("请输入", "求最小数")
+        b = InputBox("请输入", "求最小数")
+        c = InputBox("请输入", "求最小数")
+        If a < b Then min = a Else min = b
+        If c < min Then min = c
+
+        MsgBox("最小数是" & min)
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    '判断输入数
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim a As Single
+        a = InputBox("请输入", "判断输入数")
+        If a > 0 Then
+            MsgBox("a大于零")
+        ElseIf a < 0 Then
+            MsgBox("a小于零")
+        Else
+            MsgBox("a等于零")
+        End If
+
+    End Sub
+
+    '季节查询
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim nu As Integer
+        nu = InputBox("请输入月份：", "季节查询")
+        Select Case nu
+            Case 1, 2, 12
+                MsgBox("冬季", MsgBoxStyle.OkOnly, "查询结果")
+            Case 3, 4, 5
+                MsgBox("春季", MsgBoxStyle.OkOnly, "查询结果")
+            Case 6, 7, 8
+                MsgBox("夏季", MsgBoxStyle.OkOnly, "查询结果")
+            Case 9, 10, 11
+                MsgBox("秋季", MsgBoxStyle.OkOnly, "查询结果")
+            Case Else
+                MsgBox("输入错误", MsgBoxStyle.OkCancel, "查询结果")
+        End Select
+    End Sub
+End Class
